9eb34a9d9a6aacac39d5e32ef041f302fd4f3445
Philippe <philippe.h99@gmail.com>
2019-11-18 12:09:42 +0200

    added worker + update function (not working)

---
+++ InciWorker/InciWorker/Worker.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace InciWorker
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        private HttpClient client;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+            client = new HttpClient();
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                var response = await client.GetAsync("http://localhost:5002/api/orders/update");
+                if (response.IsSuccessStatusCode)
+                {
+                    _logger.LogInformation("Website working at : {time}", DateTimeOffset.Now);
+                }
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
