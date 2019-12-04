dba590c28323e7fa43b9a6f7bcb9b6dcde4daa17
Philippe <philippe.h99@gmail.com>
2019-12-04 15:56:32 +0200

    SignalR is now working

---
+++ InciCafe.Server/incicafe.api/Controllers/StatusController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using InciCafe.BLL.Service;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading;
+
+namespace InciCafe.Api.Controllers
+{
+
+    [Route("api/status")]
+    [ApiController]
+    [EnableCors("AllowOrigin")]
+
+    public class StatusController : Controller
+    {
+        // GET api/values
+        private readonly IStatusService _statusService;
+
+
+        public StatusController(IStatusService statusService)
+        {
+            _statusService = statusService;
+        }
+
+        [HttpGet]
+        [Route("coffees/{id}")]
+
+        public async Task<int> GetStatusAsync(int id, CancellationToken ct)
+        {
+            var liste = await _statusService.GetStatusAsync(id, ct);
+            return liste.Id;
+        }
+    }
+
+}
---
+++ InciCafe.Server/incicafe.bll/Service/IStatusService.cs
@@ -0,0 +1,14 @@
+﻿using InciCafe.BLL.Dto;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace InciCafe.BLL.Service
+{
+    public interface IStatusService
+    {
+        Task<StatusDto> GetStatusAsync(int id, CancellationToken ct);
+    }
+}
